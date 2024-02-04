0538e3d99671cd50b913756e4e74312e4989c58c
Robmargar <roberto.mar.garc@gmail.com>
2024-02-03 23:50:06 -0600

    Creando Card con datos de pokeapi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { Footer } from './Components/Footer'
+import { Cards } from './Components/Cards'
+import { Nav } from './Components/Nav'
+import { Intro } from './Components/Intro'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Intro/>
       <h1 class='font-mono text-9xl'> Poke-Api</h1>
+      <Nav/>
+      <Cards/>
+      <Footer/>
     </>
   )
 }
---
+++ client/src/Components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Card= () => {
+  return (
+    <div>
+        <h1> Nombre</h1>
+        <h1> Numero pokedex  </h1>
+        <h1> imagen "sprites/front_default"</h1>
+        <h1> altura</h1>
+        <h1> peso</h1>
+        <p>tipo</p>
+        <p>movimientos</p>
+        
+        
+        
+    </div>
+  )
+}
